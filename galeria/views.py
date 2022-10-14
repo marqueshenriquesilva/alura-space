d020d03a556e7548a3290414e0dfe81112c20328
Marques Henrique <marqueshenrique@outlook.com>
2022-10-14 02:00:10 -0300

    organizando template

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(Request):
-    return HttpResponse('<h1>Alura Space</h1>')
+    return render(Request, 'index.html')
